6bd2b93d0539356c4fcbbb70d3c0867caf52f17c
tiagoorietnomdias <tiagoorietnomdias@gmail.com>
2023-08-17 14:45:02 +0100

    Mini-game update: coin rendering, celebration at 6 coins, animation updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const pswitchAudio = new Audio('./assets/pswitchthemefinal.mp3');
 
 
 const elements = document.getElementsByClassName('animated-title');
-const movementFactor = 0.3; // Adjust this to control the extent of the parallax effect
+const movementFactor = 0.3; //to control the parallax effect
 const originalPositions = new Map();
 
 function handleScroll() {
@@ -48,38 +48,39 @@ function spawnCoin() {
         coinWrapper.className = 'coin-wrapper';
 
         const coinDiv = document.createElement('div');
-        const randomTranslateX = Math.random() * (window.innerWidth - 1000); // Adjust the range as needed
-        const randomTranslateY = Math.random() * (window.innerHeight - 2000); // Adjust the range as needed
+        const randomTranslateX = Math.random() * (window.innerWidth - 1000);
+        const randomTranslateY = Math.random() * (window.innerHeight - 2000); 
 
         coinWrapper.style.transform = `translate(${randomTranslateX}px, ${randomTranslateY}px)`;
         coinDiv.className = 'coin';
 
         const backArticle = document.createElement('article');
-        backArticle.className = 'back coinElement';
+        backArticle.className = 'back coin-element';
         coinDiv.appendChild(backArticle);
 
         for (let i = 0; i < 9; i++) {
             const middleArticle = document.createElement('article');
-            middleArticle.className = 'middle coinElement';
+            middleArticle.className = 'middle coin-element';
             coinDiv.appendChild(middleArticle);
         }
 
         const frontArticle = document.createElement('article');
-        frontArticle.className = 'front coinElement';
+        frontArticle.className = 'front coin-element';
         coinDiv.appendChild(frontArticle);
 
-        coinWrapper.appendChild(coinDiv); // Append coinDiv to coinWrapper
-        document.body.appendChild(coinWrapper); // Append coinWrapper to body
+        coinWrapper.appendChild(coinDiv); 
+        document.body.appendChild(coinWrapper); 
     }
 }
 
 
 
-
+let counter=0;
 
 function deleteCoin() {
     const coins = document.getElementsByClassName('coin');
     Array.from(coins).forEach(element => {
+        counter=0;
         element.style.transition = "opacity 1s";
         element.style.opacity = 0;
         element.addEventListener('transitionend', () => {
@@ -91,11 +92,12 @@ function deleteCoin() {
 
 document.addEventListener('DOMContentLoaded', function () {
     const pswitch = document.getElementById('p-switch');
-
+    
 
     pswitch.addEventListener('click', function () {
         pswitch.classList.toggle('pressed-down');
-        const coinElement = document.getElementsByClassName('coinElement');
+        const coinElement = document.getElementsByClassName('coin-element');
+        counter=0;
         ///coin catch
         Array.from(coinElement).forEach(element => {
             element.addEventListener('mouseenter', function () {
@@ -105,12 +107,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
                 var parentElement = element.parentElement;
+                // parentElement.style.backgroundImage = `url('./assets/yellowglitter.png')`;
                 parentElement.style.transition = "opacity 0.3s";
                 parentElement.style.opacity = 0;
+                
                 parentElement.style.pointerEvents = 'none';
                 parentElement.addEventListener('transitionend', () => {
-                    parentElement.remove();
+                    
+                    counter+=1;
+                    //now that parent is gone, display the number inside it
+                    const coinNumberDiv = document.createElement('div');
+                    coinNumberDiv.className='coin-number-div';
+                    const h1Element = document.createElement('h1');
+                    h1Element.textContent = counter;
+                    console.log(h1Element.textContent);
+                    coinNumberDiv.appendChild(h1Element);
+                    parentElement.parentElement.appendChild(coinNumberDiv);
+                    //if user got all the coins
+                    if(counter===6){
+                        pswitchAudio.pause();
+                        const celebrationAudio = new Audio('./assets/celebration.mp3');
+                        celebrationAudio.play();
+                        coinNumberDiv.style.animation = "scale-unscale 2s infinite";
+
+                        celebrationAudio.addEventListener('ended', () => {
+                            coinNumberDiv.style.animation = "fade-out 0.5s";
+                            coinNumberDiv.addEventListener('animationend', () => {
+                                parentElement.parentElement.remove();
+                            });
+                        });
+
+                        const highscoreWindow = document.createElement('div');
+                        highscoreWindow.className = 'highscore-window';
+                        }
+
+                    
+                    //if not
+                    else{
+                        coinNumberDiv.style.animation = "fade-out 2s 1s";
+                    coinNumberDiv.addEventListener('animationend', () => {
+                        parentElement.parentElement.remove();
+                    });
+                    }
+                    
+                    
+                
+                   
                 });
+                
+                
+
             });
         });
     });
